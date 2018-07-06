d37a966ab5931d7d4f5a9866df1015ab25fdabea
martibook <martibook@outlook.com>
2018-07-06 17:43:34 +0300

    revise

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import setup
+import setuptools
+import codecs
+import os
 
 
-setup(name='wordasso',
+def local_file(file):
+    return codecs.open(os.path.join(os.path.dirname(__file__), file), 'r', 'utf-8')
 
-      version='0.1.0',
 
-      author='Author',
-
-      author_email='martibook@outlook.com',
-
-      url='https://github.com/martibook/wordasso.git',
-
-      description='Several word association function based on datamuse API',
-
-      packages=['wordasso'])
+setuptools.setup(
+    name="wordasso",
+    version="0.1.0",
+    author="Author",
+    author_email="martibook@outlook.com",
+    description="Several word association function based on datamuse API",
+    long_description=local_file('README.rst').read(),
+    url="https://github.com/martibook/wordasso.git",
+    packages=["wordasso"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
