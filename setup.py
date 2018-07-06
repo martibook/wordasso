4add64d87dc58731129fadf39b37d74aa2ace106
martibook <martibook@outlook.com>
2018-07-06 17:06:35 +0300

    add configuration files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='wordasso',
+
+      version='0.1.0',
+
+      author='Author',
+
+      author_email='martibook@outlook.com',
+
+      url='https://github.com/martibook/wordasso.git',
+
+      description='Several word association function based on datamuse API',
+
+      packages=['wordasso'])
