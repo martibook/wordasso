014e48d8c266b3a3734b13b734837be96c4b5f7d
martibook <martibook@outlook.com>
2018-07-06 19:35:55 +0300

    fix install requirements

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ def local_file(file):
     return codecs.open(os.path.join(os.path.dirname(__file__), file), 'r', 'utf-8')
 
 
+install_reqs = [line.strip() for line in local_file('requirements.txt').readlines() if line.strip() != '']
+
+
 setuptools.setup(
     name="wordasso",
-    version="0.1.0",
+    version="0.1.1",
     author="Author",
     author_email="martibook@outlook.com",
     description="Several word association function based on datamuse API",
-    long_description=local_file('README.rst').read(),
+    long_description=local_file('README.md').read(),
     url="https://github.com/martibook/wordasso.git",
+    install_requires=install_reqs,
     packages=["wordasso"],
     classifiers=(
         "Programming Language :: Python :: 3",
